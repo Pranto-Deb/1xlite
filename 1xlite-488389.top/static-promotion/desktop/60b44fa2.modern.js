b72dcc8a92a2fe5292254b21e11deed103ea91c0
Medisquare Dev1003 <mediswe1003@gmail.com>
2024-12-21 13:34:23 +0600

    update

--- 1xlite-488389.top/static-promotion/desktop/60b44fa2.modern.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[318],{4529:function(n,e,t){"use strict";t.r(e);var l={name:"PromotionPage"},o=t(14),component=Object(o.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("NuxtChild")}),[],!1,null,null,null);e.default=component.exports}}]);
